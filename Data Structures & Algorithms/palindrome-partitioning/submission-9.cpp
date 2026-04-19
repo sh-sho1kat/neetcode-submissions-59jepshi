20745c670abd9904306cb2de52a87b11aa728817
Shefat Hossen Shoikat <109766125+sh-sho1kat@users.noreply.github.com>
2026-04-20 00:39:04 +0600

    Add: palindrome-partitioning - submission-9

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-9.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<string>>res;
+    bool dp[21][21]={};
+    void func(int start, int pos, int n,string &s, vector<string> &v)
+    {
+        if(pos==n)
+        {
+            if(start==n-1)
+                res.push_back(v);
+            return;
+        }
+        func(start,pos+1,n,s,v);
+        if(dp[start+1][pos])
+        {
+            v.push_back(s.substr(start+1,pos-start));
+            func(pos,pos+1,n,s,v);
+            v.pop_back();
+        }
+        return;
+
+    }
+    vector<vector<string>> partition(string s) {
+        for(int i = s.size()-1;i>=0;i--)
+        {
+            for(int j = i;j<s.size();j++)
+            {
+                dp[i][j] = s[i]==s[j] & (j-i<=2 || dp[i+1][j-1]);      
+            }
+        }
+        vector<string>v;
+        func(-1,0,s.size(),s,v);
+        return res;
+    }
+};

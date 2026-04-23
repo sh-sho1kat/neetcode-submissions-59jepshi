47d7df472da9d3e1fa5b586d64016b957426f61b
Shefat Hossen Shoikat <109766125+sh-sho1kat@users.noreply.github.com>
2026-04-24 04:39:04 +0600

    Add: edit-distance - submission-2

---
+++ Data Structures & Algorithms/edit-distance/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+
+        int dp[105][105]={};
+
+        for(int i = 0;i<=m;i++)
+            dp[i][n] = m-i;
+        for(int i = 0;i<=n;i++)
+            dp[m][i] = n-i;
+
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int j = n-1;j>=0;j--)
+            {
+                if(word1[i]==word2[j])
+                    dp[i][j] = dp[i+1][j+1];
+                else
+                    dp[i][j] = min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]})+1;
+            }
+        }
+        return dp[0][0];
+    }
+};

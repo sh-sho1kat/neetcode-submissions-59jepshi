387eebd88a42d23c04c214b46ebfa5a08e99c17e
Shefat Hossen Shoikat <109766125+sh-sho1kat@users.noreply.github.com>
2026-05-04 10:03:38 +0600

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int mx = -1000;
+        for(int i = 0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            mx = max(mx,sum);
+            if(sum<0)
+            {
+                sum = 0;
+            }
+        }
+        return mx;
+    }
+};

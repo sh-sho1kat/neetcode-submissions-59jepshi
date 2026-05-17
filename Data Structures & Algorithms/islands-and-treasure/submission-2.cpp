e6d0724874f6b70c84b786fb303ea93d7569a6e2
Shefat Hossen Shoikat <109766125+sh-sho1kat@users.noreply.github.com>
2026-05-18 00:15:54 +0600

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        queue<pair<int,pair<int,int>>> q;
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        bool flag = true;
+        for(int i =0;i<m;i++)
+            for(int j = 0;j<n;j++)
+            {
+                if(grid[i][j]==0)
+                {
+                    q.push({grid[i][j],{i,j}});
+                    flag = false;
+                }
+            }
+        if(flag)
+            return;
+        
+        while(!q.empty())
+        {
+            int val = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
+
+            q.pop();
+
+            if(row+1<m)
+            {
+                if(grid[row+1][col]>val)
+                {
+                    grid[row+1][col] = val+1;
+                    q.push({grid[row+1][col],{row+1,col}});
+                }
+            }
+            if(row-1>=0)
+            {
+                if(grid[row-1][col]>val)
+                {
+                    grid[row-1][col] = val+1;
+                    q.push({grid[row-1][col],{row-1,col}});
+                }
+            }
+            if(col+1<n)
+            {
+                if(grid[row][col+1]>val)
+                {
+                    grid[row][col+1] = val+1;
+                    q.push({grid[row][col+1],{row,col+1}});
+                }
+            }
+            if(col-1>=0)
+            {
+                if(grid[row][col-1]>val)
+                {
+                    grid[row][col-1] = val+1;
+                    q.push({grid[row][col-1],{row,col-1}});
+                }
+            }
+        }
+    }
+};
